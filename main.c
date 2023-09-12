1efea520b22d7563ae2811789b1af89e4057fc71
hmz-rhl <77698738+hmz-rhl@users.noreply.github.com>
2023-09-12 23:30:24 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+void say(char* text){
+    
+    int size = sizeof(text)/text[0];
+    char *buffer = calloc(size, sizeof(text));
+    if(buffer == NULL){
+        exit(1);
+    }
+    int index = 0;
+    char start;
+
+    char letter = text[index];
+    while(letter != '\0'){
+
+        if(letter >= 'a' && letter <= 'z') start = 'a';
+        else if (letter >= 'a' && letter <= 'z') start = 'A';
+        else{
+            buffer[index] = letter;
+            printf("\r%s", buffer);
+            fflush(stdout);
+            index++;
+            letter = text[index];
+            continue;
+        }
+        
+
+        for(char i = start;  i < letter; i++){
+
+            //printf("\r");
+            buffer[index] = i;
+            printf("\r%s", buffer);
+            fflush(stdout);
+            usleep(50000);
+        }
+    
+        buffer[index] = letter;
+        printf("\r%s", buffer);
+        fflush(stdout);
+        index++;
+        letter = text[index];
+        
+    }
+    putchar('\n');
+    free(buffer);
+    return;
+}
+
+int main()
+{
+    say("Hello World !");
+    return 0;
+}
